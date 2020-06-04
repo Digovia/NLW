be8dc27d2f0ef18381e4d41957e56524d946a2bd
Rodrigo Fraga <digo_via@hotmail.com>
2020-06-04 00:11:20 -0300

    Aula 3, updates

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -52,7 +52,7 @@ class PointsCpontroller {
         const trx = await knex.transaction();
     
         const point = {
-            image: 'image-fake',
+            image: 'https://images.unsplash.com/photo-1508830524289-0adcbe822b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
             name,
             email,
             whatsapp,
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import express, { json } from 'express';
 import path from 'path';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
